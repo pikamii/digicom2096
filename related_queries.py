69121e8b5dbce342c115e63c3d1d0137eeba02cd
mii <mii@mii-MacBook.local>
2019-08-20 22:19:52 +0900

    update

--- related_queries.py
+++ related_queries.py
@@ -9,7 +9,7 @@ else:
     kw_list = ["カフェ","現代アート"]
 
 pytrends = TrendReq(hl='ja-JP', tz=360)
-pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='JP', gprop='')
+pytrends.build_payload(kw_list, cat=23, timeframe='today 5-y', geo='JP', gprop='')
 
 r = pytrends.related_queries()
 
